e93d880a9a3eb7457ed9a0cff3b7705ce0b95607
yuki_ishii <mizuki.izuna@gmail.com>
2020-05-10 04:05:13 +0900

    チュートリアル２まで

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+mod pong;
+
+use crate::pong::Pong;
+
+use amethyst::{
+    core::TransformBundle,
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+
+
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+
+    let assets_dir = app_root.join("assets");
+    let config_dir = app_root.join("config");
+    let display_config_path = config_dir.join("display.ron");
+
+    let game_data = GameDataBuilder::default()
+        // Add the transform bundle which handles tracking entity positions
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                // The RenderToWindow plugin provides all the scaffolding for opening a window and
+                // drawing on it
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                // RenderFlat2D plugin is used to render entities with `SpriteRender` component.
+                .with_plugin(RenderFlat2D::default()),
+        )?;
+
+    let mut game = Application::new(assets_dir, Pong, game_data)?;
+    game.run();
+
+    Ok(())
+}
